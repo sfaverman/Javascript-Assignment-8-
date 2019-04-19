7605aedaae013a028c3f1bcffa1093ad54057644
sfaverman <sofiasd@yahoo.com>
2019-04-18 20:07:26 -0700

    Completed Part 1 - Bank Account Application

--- bankaccount/js/script.js
+++ bankaccount/js/script.js
@@ -1,2 +1,32 @@
 /*eslint-env browser*/
+var $ = function (id) {
+    "use strict";
+    return window.document.getElementById(id);
+};
+
+var ownerName = "John", depositAmount ="$25,000", withdrawAmount;
+window.addEventListener("load", function () {
+    "use strict";
+
+    $("btnName").onclick = function() {
+        "use strict";
+        ownerName = window.prompt('Enter your name');
+        window.console.log(ownerName);
+    };
+    $("btnDeposit").onclick = function() {
+        "use strict";
+        depositAmount = window.prompt('Enter Deposit Amount','999.99');
+        window.console.log(depositAmount);
+
+    };
+    $("btnWithdraw").onclick = function() {
+        "use strict";
+        withdrawAmount = window.prompt('Enter Withdraw Amount','999.99');
+        window.console.log(withdrawAmount);
+    };
+
+    $("display").innerHTML = ownerName + " has a balance of "  + depositAmount;
+
+});
+
 
